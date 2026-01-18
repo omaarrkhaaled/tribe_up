d44b5a5daf7abd813464605c06c1676b774aaead
omar khaled <omarkhaled10102004@gmail.com>
2026-01-18 08:49:47 +0200

    chore : configure app themes and constants

---
+++ lib/core/resources/assets_managar.dart
@@ -0,0 +1,10 @@
+class ImageAssets {
+  static const String imagePath = 'assets/images';
+  static const String examImage='$imagePath/exam_image.png';
+}
+class AnimationAssets {
+  static const String animationPath = 'assets/animations';
+  static const String loadingAnimation='$animationPath/loading.json';
+  static const String welcomeAnimation='$animationPath/welcome.json';
+  
+}
