a83004f20e303df83de2817bbb93e2f8e5284778
omar khaled <omarkhaled10102004@gmail.com>
2026-01-20 19:40:29 +0200

    feat : implement domain layer ( the repo contract and the use case with its entities)

---
+++ lib/features/auth/sign_up/domain/entities/sign_up_response/sign_up_response_entity.dart
@@ -0,0 +1,15 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'sign_up_response_entity.g.dart';
+
+@JsonSerializable()
+class SignUpResponseEntity {
+   final String? accessToken;
+  final String? refreshToken;
+
+  const SignUpResponseEntity({this.accessToken, this.refreshToken});
+  
+  factory SignUpResponseEntity.fromJson(Map<String, dynamic> json) =>
+      _$SignUpResponseEntityFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SignUpResponseEntityToJson(this);
+}
