541be1edeb7f709ff05bd906455bc0b7956bb673
omar khaled <omarkhaled10102004@gmail.com>
2026-01-24 15:15:53 +0200

    feat  :  implement the api and data layers ( local and remote )

---
+++ lib/features/auth/login/data/data_sources/login_local_data_source.dart
@@ -0,0 +1,8 @@
+import 'package:tribe_up/config/base_response/base_response.dart';
+
+abstract class LoginLocalDataSource {
+  Future<BaseResponse<void>> saveTokens({
+    required String token,
+    required String refreshToken,
+  });
+}
