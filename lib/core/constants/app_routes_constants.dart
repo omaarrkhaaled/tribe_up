e60b60eedfe09b48c26f75b3ca4a766eaa32f644
omar khaled <omarkhaled10102004@gmail.com>
2026-02-05 12:07:19 +0200

    feat(change password): Implement the routing and add the constants

--- lib/core/constants/app_routes_constants.dart
+++ lib/core/constants/app_routes_constants.dart
@@ -8,6 +8,6 @@ abstract class AppRoutesConstants {
   static const String profile = '/profile';
   static const String editProfile = '/editProfile';
   static const String changePassword = '/changePassword';
-
+  static const String verifyEmail = '/verifyEmail';
 }
 
