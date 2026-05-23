10f10a30296a38a1dd4ced0e964dbe071fc6695f
omar khaled <omarkhaled10102004@gmail.com>
2026-05-23 23:42:29 +0300

    feat(groups): Refactor GroupsScreen to use TribesScreen and add new widgets for search and tribes list

--- lib/features/feed/presentation/view/screens/groups_screen.dart
+++ lib/features/feed/presentation/view/screens/groups_screen.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:tribe_up/core/constants/app_routes_constants.dart';
+import 'package:tribe_up/features/groups/presentation/view/screens/tribes_screen.dart';
 
 class GroupsScreen extends StatelessWidget {
   const GroupsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          context.pushNamed(AppRoutesConstants.editProfile);
-        },
-        child: const Text('go To Edit Profile'),
-      ),
-    );
+    return const TribesScreen();
   }
 }
