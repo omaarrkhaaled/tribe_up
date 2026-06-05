5fef2c754402d186d3e8ea1bf10289945dd3a99f
omar khaled <omarkhaled10102004@gmail.com>
2026-06-05 22:46:16 +0300

    feat(feed): Update editPost method to return PostEntity and add id to MediaEntity

--- lib/features/feed/domain/entities/media_entity.dart
+++ lib/features/feed/domain/entities/media_entity.dart
@@ -1,9 +1,11 @@
 class MediaEntity {
+  final int id;
   final String mediaURL;
   final String mediaType;
   final int order;
 
   const MediaEntity({
+    required this.id,
     required this.mediaURL,
     required this.mediaType,
     required this.order,
