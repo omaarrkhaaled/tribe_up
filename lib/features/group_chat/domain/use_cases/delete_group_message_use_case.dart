19a20a9f151697dde7e5d5b46b4ce3851b11f8d0
omar khaled <omarkhaled10102004@gmail.com>
2026-06-08 22:11:18 +0300

    feat(group_chat): Add use cases for sending, editing, deleting messages and retrieving chat inbox and group messages

---
+++ lib/features/group_chat/domain/use_cases/delete_group_message_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:injectable/injectable.dart';
+import 'package:tribe_up/config/base_response/base_response.dart';
+import 'package:tribe_up/features/group_chat/domain/repositories/group_chat_repository.dart';
+
+@injectable
+class DeleteGroupMessageUseCase {
+  final GroupChatRepository _repository;
+
+  DeleteGroupMessageUseCase(this._repository);
+
+  Future<BaseResponse<void>> call(int messageId) {
+    return _repository.deleteMessage(messageId);
+  }
+}
