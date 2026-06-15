51263ced43220632aae472acb4700e6f4e6d8a49
omar khaled <omarkhaled10102004@gmail.com>
2026-06-15 22:15:13 +0300

    refactor(assets): Remove unused welcomeAnimation from AnimationAssets class

--- lib/core/resources/assets_managar.dart
+++ lib/core/resources/assets_managar.dart
@@ -7,6 +7,5 @@ class ImageAssets {
 class AnimationAssets {
   static const String animationPath = 'assets/animations';
   static const String loadingAnimation = '$animationPath/loading.json';
-  static const String welcomeAnimation = '$animationPath/welcome.json';
   static const String trailLoading = '$animationPath/trail_loading.json';
 }
