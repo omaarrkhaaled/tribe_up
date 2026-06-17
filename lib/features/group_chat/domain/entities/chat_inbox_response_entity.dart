f862b56f0c0e0951abfb56a0db07c59f08b222c2
omar khaled <omarkhaled10102004@gmail.com>
2026-06-17 19:54:33 +0300

    feat(chat): Simplify ChatInboxResponseEntity and update repository methods for improved API consistency

--- lib/features/group_chat/domain/entities/chat_inbox_response_entity.dart
+++ lib/features/group_chat/domain/entities/chat_inbox_response_entity.dart
@@ -2,14 +2,6 @@ import 'package:tribe_up/features/group_chat/domain/entities/chat_inbox_item_ent
 
 class ChatInboxResponseEntity {
   final List<ChatInboxItemEntity> items;
-  final int currentPage;
-  final int totalPages;
-  final int totalItems;
 
-  const ChatInboxResponseEntity({
-    required this.items,
-    required this.currentPage,
-    required this.totalPages,
-    required this.totalItems,
-  });
+  const ChatInboxResponseEntity({required this.items});
 }
