cc5fa2be5f9f1a7de0fe540b2f3ac4fac4b97360
omar khaled <omarkhaled10102004@gmail.com>
2026-07-22 20:47:28 +0300

    feat(auth): Enhance password fields with visibility toggle

--- lib/features/auth/presentation/widgets/shared/password_field_widget.dart
+++ lib/features/auth/presentation/widgets/shared/password_field_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tribe_up/core/constants/ui_constants.dart';
 
-class PasswordFieldWidget extends StatelessWidget {
+class PasswordFieldWidget extends StatefulWidget {
   final TextEditingController password;
   final void Function(String) onChanged;
 
@@ -12,18 +12,33 @@ class PasswordFieldWidget extends StatelessWidget {
   });
 
   @override
+  State<PasswordFieldWidget> createState() => _PasswordFieldWidgetState();
+}
+
+class _PasswordFieldWidgetState extends State<PasswordFieldWidget> {
+  bool _obscure = true;
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TextFormField(
-          controller: password,
-          obscureText: true,
-          decoration: const InputDecoration(
+          controller: widget.password,
+          obscureText: _obscure,
+          decoration: InputDecoration(
             labelText: UiConstants.password,
             hintText: UiConstants.enterPassword,
             helperText: '',
+            suffixIcon: IconButton(
+              icon: Icon(
+                _obscure
+                    ? Icons.visibility_off_outlined
+                    : Icons.visibility_outlined,
+              ),
+              onPressed: () => setState(() => _obscure = !_obscure),
+            ),
           ),
-          onChanged: onChanged,
+          onChanged: widget.onChanged,
         ),
       ],
     );
--- lib/features/auth/presentation/widgets/sign_up/password_fields_widget.dart
+++ lib/features/auth/presentation/widgets/sign_up/password_fields_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tribe_up/core/constants/ui_constants.dart';
 import 'package:tribe_up/core/utils/validator.dart';
 
-class PasswordFieldsWidget extends StatelessWidget {
+class PasswordFieldsWidget extends StatefulWidget {
   final TextEditingController password;
   final TextEditingController confirmPassword;
   final void Function(String) onPasswordChanged;
@@ -17,33 +17,60 @@ class PasswordFieldsWidget extends StatelessWidget {
   });
 
   @override
+  State<PasswordFieldsWidget> createState() => _PasswordFieldsWidgetState();
+}
+
+class _PasswordFieldsWidgetState extends State<PasswordFieldsWidget> {
+  bool _obscurePassword = true;
+  bool _obscureConfirm = true;
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TextFormField(
-          controller: password,
+          controller: widget.password,
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          obscureText: true,
-          decoration: const InputDecoration(
+          obscureText: _obscurePassword,
+          decoration: InputDecoration(
             labelText: UiConstants.password,
             hintText: UiConstants.enterPassword,
             helperText: '',
+            suffixIcon: IconButton(
+              icon: Icon(
+                _obscurePassword
+                    ? Icons.visibility_off_outlined
+                    : Icons.visibility_outlined,
+              ),
+              onPressed: () =>
+                  setState(() => _obscurePassword = !_obscurePassword),
+            ),
           ),
           validator: Validator.validatePassword,
-          onChanged: onPasswordChanged,
+          onChanged: widget.onPasswordChanged,
         ),
         SizedBox(height: 8),
         TextFormField(
-          controller: confirmPassword,
+          controller: widget.confirmPassword,
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          obscureText: true,
-          decoration: const InputDecoration(
+          obscureText: _obscureConfirm,
+          decoration: InputDecoration(
             labelText: UiConstants.confirmPassword,
             hintText: UiConstants.confirmPassword,
             helperText: '',
+            suffixIcon: IconButton(
+              icon: Icon(
+                _obscureConfirm
+                    ? Icons.visibility_off_outlined
+                    : Icons.visibility_outlined,
+              ),
+              onPressed: () =>
+                  setState(() => _obscureConfirm = !_obscureConfirm),
+            ),
           ),
-          validator: (v) => Validator.validateConfirmPassword(v, password.text),
-          onChanged: onConfirmPasswordChanged,
+          validator: (v) =>
+              Validator.validateConfirmPassword(v, widget.password.text),
+          onChanged: widget.onConfirmPasswordChanged,
         ),
       ],
     );
